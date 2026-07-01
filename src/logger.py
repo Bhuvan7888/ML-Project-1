e6016ac221b7b9d89850a1cb6411376c052d3be6
Bhuvan7888 <bhuvanmalhotra11@gmail.com>
2026-07-01 14:45:42 +0530

    EDA and Problem Statement

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,5 @@
 import logging  ## any execeution that happens you are able to log that
 import os
-
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
